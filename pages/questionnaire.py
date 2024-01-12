18f0f99e81ebbb9e8711ad9fbc9843788529c3d4
MatheusFeijoo <matheuspuime@gmail.com>
2024-01-11 23:31:38 -0300

    Update questionnaire.py

--- pages/questionnaire.py
+++ pages/questionnaire.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-
 st.title("Questionnaire")
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    index=None,
+)
